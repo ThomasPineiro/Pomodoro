bee81924e0cd3f790cb2cd25801076c15cdc9255
Thomas <onixyros@gmail.com>
2018-11-26 21:28:28 +0100

    added list, need to stylise

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-var minute, second = 0; 
+var minute, second = 0;
+var newTask = False;
 
 //On Start click
 $("#play-btn").click(function(){
+    task = $("#tache").val();
     minute = $("#minutes").val();
     second = $("#seconds").val();
     //Using a variable with setInterval in order to use the clearInterval method
@@ -19,6 +21,8 @@ $("#play-btn").click(function(){
                 $("#countdown").html('<p>'+minute.toString().padStart(2, '0')+':'+second.toString().padStart(2, '0')+'</p>');
                 if (minute == 0 & second == 0) {
                     clearInterval(countdown);
+                    //A remplacer par append si possible pour garder toute les taches
+                    $("#listField").html('<ul>'+task+'</ul>');
                 }
             }
         , 1000)
